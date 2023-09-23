3855a33d7a9d80b763ce911c4f620741571d5f35
Kobe-Jones <yakovsap@gmail.com>
2023-09-23 23:54:46 +0300

    nu yalla

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+
 
 def sayHello(request):
     return HttpResponse('Hello World')
@@ -27,3 +28,9 @@ class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
