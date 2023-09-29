4d59d20f93574d4da2d3948521b574d98c98b5bb
Kobe-Jones <yakovsap@gmail.com>
2023-09-30 00:01:09 +0300

    /////////////////

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,11 +1,23 @@
 from django.contrib import admin 
-from django.urls import path 
-from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('menu/items/', views.MenuItemsView.as_view(), name='menu-items'),
     path('menu/items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
     path('api-token-auth/', obtain_auth_token),
+    path('about/', views.about, name="about"),
+    path('menu/', views.Menu, name="menu"),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
+    # API paths
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
+    path('booking/', include(router.urls)),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,6 @@
+from django.core import serializers
+from datetime import datetime
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
@@ -5,7 +8,7 @@ from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
@@ -22,3 +25,12 @@ class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+def about(request):
+    return render(request, 'about.html')
+
+def reservations(request:HttpRequest) -> HttpResponse:
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'reservations.html',{"bookings":booking_json})
